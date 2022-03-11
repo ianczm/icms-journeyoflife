8adfb3e20b33897fde7d7d1a5eb19ba97a08e718
ianczm <ianczm@live.co.uk>
2022-03-11 18:31:44 +0800

    Add firebase and axios: fix naming

--- src/main.js
+++ src/main.js
@@ -23,8 +23,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
 
 
 
