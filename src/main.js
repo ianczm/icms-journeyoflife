dacbabe491280dab5fbb57278244a889e1fecacd
ianczm <ianczm@live.co.uk>
2022-03-11 18:28:52 +0800

    Add firebase and axios

--- src/main.js
+++ src/main.js
@@ -2,8 +2,32 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAvrnKMuhBbsFh6sWx-cLksgUPPAlXnJFU",
+  authDomain: "icms-journeyoflife.firebaseapp.com",
+  projectId: "icms-journeyoflife",
+  storageBucket: "icms-journeyoflife.appspot.com",
+  messagingSenderId: "679695129854",
+  appId: "1:679695129854:web:85600f9d9a1e6fca7999af",
+  measurementId: "G-1HMC0X472X"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+
 const app = createApp(App)
-
 app.use(router)
-
 app.mount('#app')
